80855c27372af420d83b3dcbc32e587d6f50d8f5
romantic123fly <853394528@qq.com>
2020-04-02 19:45:22 +0800

    no message

--- Assets/HSJProject/MySqlDemo/HsjMySQLInstance.cs
+++ Assets/HSJProject/MySqlDemo/HsjMySQLInstance.cs
@@ -14,7 +14,7 @@ using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
 
-public class MySqlAccess
+public class HsjMySQLInstance
 {
     private static MySqlConnection mySqlConnection;
     private static string host;
@@ -22,7 +22,13 @@ public class MySqlAccess
     private static string userName;
     private static string password;
     private static string databaseName;
-
+    //grant all on *.* to hsj@'%' identified by 'romantic123456' with grant option; 
+    //grant all privileges on *.* to 'hsj'@'%' identified by 'romantic123456' with grant option;
+    //GRANT ALL PRIVILEGES ON *.* TO 'hsj'@'%' IDENTIFIED BY 'romantic123456';
+    //grant all privileges on *.* to 'root'@'%' identified by 'romantic123456' with grant option;  
+    //flush privileges;
+    //1.set global validate_password_policy=0;
+//2.grant all privileges on *.* to 'root'@'%'identified by 'Aa123456' with grant option;
     /// <summary>
     /// 构造方法
     /// </summary>
@@ -30,19 +36,16 @@ public class MySqlAccess
     /// <param name="_userName">用户名</param>
     /// <param name="_password">密码</param>
     /// <param name="_databaseName">数据库名称</param>
-    public MySqlAccess(string _host, string _port, string _userName, string _password, string _databaseName)
+    public HsjMySQLInstance(string _host, string _port, string _userName, string _password, string _databaseName)
     {
         host = _host;
         port = _port;
         userName = _userName;
         password = _password;
         databaseName = _databaseName;
-        OpenSql();
     }
 
-    /// <summary>
     /// 打开数据库
-    /// </summary>
     public void OpenSql()
     {
         try
@@ -56,12 +59,9 @@ public class MySqlAccess
         {
             throw new Exception("连接失败，请检查MySql服务是否打开：" + e.Message.ToString());
         }
-
     }
 
-    /// <summary>
     /// 关闭数据库
-    /// </summary>
     public void CloseSql()
     {
         if (mySqlConnection != null)
@@ -76,32 +76,73 @@ public class MySqlAccess
     /// <summary>
     /// 查询数据
     /// </summary>
-    /// <param name="tableName">表名</param>
     /// <param name="items">要查询的列</param>
     /// <param name="whereColumnName">查询的条件列</param>
     /// <param name="operation">条件操作符</param>
     /// <param name="value">条件的值</param>
     /// <returns></returns>
-    public DataSet Select(string tableName, string[] items, string[] whereColumnName, string[] operation, string[] value)
+    public DataSet Select(string tableName, string[] whereColumnName, string[] operation, string[] value)
     {
         if (whereColumnName.Length != operation.Length || operation.Length != value.Length)
         {
             throw new Exception("输入不正确：" + "要查询的条件、条件操作符、条件值 的数量不一致！");
         }
-        string query = "Select " + "*";
-        //for (int i = 1; i < items.Length; i++)
-        //{
-        //    query += "," + items[i];
-        //}
-
-        query += " FROM " + items[0] + " WHERE " + whereColumnName[0] + " " + operation[0] + " '" + value[0] + "'";
+        string querySQL = "Select " + "*";
+        querySQL += " FROM " + tableName + " WHERE " + whereColumnName[0] + " " + operation[0] + " '" + value[0] + "'";
         for (int i = 1; i < whereColumnName.Length; i++)
         {
-            query += " and " + whereColumnName[i] + " " + operation[i] + " '" + value[i] + "'";
+            querySQL += " and " + whereColumnName[i] + " " + operation[i] + " '" + value[i] + "'";
         }
-        Debug.Log(query);
-        return QuerySet(query);
+        Debug.Log(querySQL);
+        return QuerySet(querySQL);
     }
+
+    /// <summary>
+    /// 添加数据
+    /// </summary>
+    /// <param name="tableName">表名</param>
+    /// <param name="userName">用户名</param>
+    /// <param name="password">密码</param>
+    /// INSERT INTO `mydata`.`userinfo` (`id`, `username_m`, `password_m`) VALUES ('3', 'd', 'dd');
+    /// <returns></returns>
+    public DataSet Add(string tableName, string userName,string password)
+    {
+        string addSQL = "INSERT INTO `mydata`.`userinfo` (`username_m`, `password_m`) VALUES " + "('" + userName + "','" + password + "')";
+        Debug.Log(addSQL);
+        return QuerySet(addSQL);
+    }
+
+    /// <summary>
+    /// 删除数据
+    /// </summary>
+    /// <param name="tableName">表名</param>
+    /// <param name="id">唯一主键</param>
+    ///DELETE FROM `mydata`.`userinfo` WHERE (`id` = '4');
+    /// <returns></returns>
+    public DataSet Delete(string tableName, string id)
+    {
+        string deleteSQL = "DELETE FROM `mydata`.`"+tableName+"` WHERE (`id` = '"+ id +"');";
+        Debug.Log(deleteSQL);
+        return QuerySet(deleteSQL);
+    }
+
+    /// <summary>
+    /// 更新数据
+    /// </summary>
+    /// <param name="tableName"></param>
+    /// <param name="userName"></param>
+    /// <param name="password"></param>
+    /// <param name="id"></param>
+    ///UPDATE `mydata`.`userinfo` SET `username_m` = 'g', `password_m` = 'gg' WHERE (`id` = '10');
+    /// <returns></returns>
+    public DataSet Update(string tableName, string userName, string password,string id)
+    {
+        string updateSQL = "UPDATE `"+ tableName + "` SET `username_m` = '"+ userName + "', `password_m` = '"+password+"' WHERE (`id` = '"+id+"')";
+        Debug.Log(updateSQL);
+      
+        return QuerySet(updateSQL);
+    }
+
 
     /// <summary>
     /// 执行SQL语句
---
+++ Assets/HSJProject/MySqlDemo/UserLoginView.cs
@@ -0,0 +1,152 @@
+﻿#region 模块信息
+// **********************************************************************
+// Copyright (C) 2020 
+// Please contact me if you have any questions
+// File Name:             UserLogin
+// Author:                幻世界
+// QQ:                    853394528 
+// **********************************************************************
+#endregion
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UserLoginView : MonoBehaviour
+{
+    public InputField userNameInput;
+    public InputField passwordInput;
+    
+    public Text loginMessage;//提示用户登录信息
+    public Button loginBtn;//登录
+    public Button resigerBtn;//注册
+    public Button logoutBtn;//注销
+    public Button changepasswordBtn;//注销
+    public string host; //IP地址
+    public string port;//端口号
+    public string userName; //用户名
+    public string password; //密码
+    private string dataSchemasName;//数据库名称
+    private HsjMySQLInstance mysql;//封装好的数据库类
+    private string userInfoID;
+    private void Start()
+    {
+        dataSchemasName = "mydata";
+        mysql = new HsjMySQLInstance(host, port, userName, password, dataSchemasName);
+        loginBtn.onClick.AddListener(Login);
+        resigerBtn.onClick.AddListener(Resigion);
+        logoutBtn.onClick.AddListener(Logout);
+        changepasswordBtn.onClick.AddListener(ChangePassword);
+    }
+    /// <summary>
+    /// 按下登录按钮
+    /// </summary>
+    private void Login()
+    {
+        mysql.OpenSql();
+        DataSet ds = mysql.Select( "userinfo" , new string[] { "`" + "username_m" + "`", "`" + "password_m" + "`" }, new string[] { "=", "=" }, new string[] { userNameInput.text, passwordInput.text });
+        if (ds != null)
+        {
+            DataTable table = ds.Tables[0];   
+            if (table.Rows.Count > 0)
+            {
+                loginMessage.text = "登陆成功！";
+                loginMessage.color = Color.green;
+                Debug.Log("用户id：" + table.Rows[0][0]);
+                userInfoID = table.Rows[0][0].ToString();
+            }
+            else
+            {
+                loginMessage.text = "用户名或密码错误！";
+                loginMessage.color = Color.red;
+            }
+        }
+        mysql.CloseSql();
+    }
+    private void Resigion()
+    {
+        mysql.OpenSql();
+        DataSet ds = mysql.Add("userinfo", userNameInput.text, passwordInput.text );
+        if (ds != null)
+        {
+            loginMessage.text = "注册成功！";
+            loginMessage.color = Color.green;
+        }
+        else
+        {
+            loginMessage.text = "注册成功！";
+            loginMessage.color = Color.red;
+        }
+        mysql.CloseSql();
+    }
+    private void Logout()
+    {
+        if (userInfoID == "" || userInfoID == null)
+        {
+            loginMessage.text = "请用户先登录后才能注销！";
+            Debug.Log("请用户先登录后才能注销");
+            return;
+        }
+        mysql.OpenSql();
+        DataSet ds = mysql.Delete("userinfo", userInfoID);
+        try
+        {
+            if (ds != null && ds.HasErrors == false)
+            {
+                loginMessage.text = "注销成功！";
+                loginMessage.color = Color.green;
+            }
+            else
+            {
+
+                loginMessage.text = "注销失败！";
+                loginMessage.color = Color.red;
+            }
+        }
+        catch (System.Exception e)
+        {
+
+            throw new System.Exception(e.Message);
+        }
+       
+        mysql.CloseSql();
+    }
+    private void ChangePassword()
+    {
+        if (userInfoID == "" || userInfoID == null)
+        {
+            loginMessage.text = "请用户先登录后才能改密！";
+            Debug.Log("请用户先登录后才能改密");
+            return;
+        }
+        mysql.OpenSql();
+        DataSet ds = mysql.Update("userinfo", userNameInput.text, passwordInput.text,userInfoID);
+        try
+        {
+            if (ds != null && ds.HasErrors == false)
+            {
+                loginMessage.text = "注销成功！";
+                loginMessage.color = Color.green;
+            }
+            else
+            {
+
+                loginMessage.text = "注销失败！";
+                loginMessage.color = Color.red;
+            }
+        }
+        catch (System.Exception e)
+        {
+
+            throw new System.Exception(e.Message);
+        }
+
+        mysql.CloseSql();
+    }
+    private void OnDestroy()
+    {
+        mysql.CloseSql();
+    }
+}
